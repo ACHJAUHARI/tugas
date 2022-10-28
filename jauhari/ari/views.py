9442874627af9d3809fcdf98f8754b3ab1d743ba
ACHJAUHARI <107332310+ACHJAUHARI@users.noreply.github.com>
2022-10-28 22:37:44 +0700

    Add files via upload

---
+++ jauhari/ari/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def ari(request):
+    return render(request,'ari.html')
---
+++ jauhari/jauhari/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def jauhari(request):
+    return render(request,'jauhari.html')
